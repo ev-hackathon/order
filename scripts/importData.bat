631617408567d8bdcede1455322ab2a87af8b94e
RSR <SrinivasaRaghavan_R@ad.infosys.com>
2016-07-29 15:54:22 +0530

    add order

---
+++ scripts/importData.bat
@@ -0,0 +1,3 @@
+mongoimport --collection Vendor --db db --file ..\data\Vendor.json
+mongoimport --collection Customer --db db --file ..\data\Customer.json
+mongoimport --collection Dictionary --db db --file ..\data\Dictionary.json
