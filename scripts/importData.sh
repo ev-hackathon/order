e7e21ae15f4ca2f7e7fc135e4452ac0b7f1b57f0
P K Tippa <pktippa@gmail.com>
2016-07-29 08:59:29 +0000

    Added scripts

---
+++ scripts/importData.sh
@@ -0,0 +1,3 @@
+mongoimport --collection Vendor --db db --file ../seed/Vendor.json
+mongoimport --collection Customer --db db --file ../seed/Customer.json
+mongoimport --collection Dictionary --db db --file ../seed/Dictionary.json
